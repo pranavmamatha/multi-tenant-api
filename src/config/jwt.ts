147f0d0d1da9132aec241871f56e3efbbc8ad2eb
pranavmamatha <pranavranjithkannan@gmail.com>
2026-02-21 20:29:42 +0530

    functions to generate tokens and functions to verify tokens

---
+++ src/config/jwt.ts
@@ -0,0 +1,43 @@
+import * as jwt from "jsonwebtoken"
+import { env } from "../config/env"
+import type { StringValue } from "ms"
+import { Role } from "../generated/prisma/client"
+import { ApiError } from "./ApiError"
+
+export type JwtPayload = {
+  userId: string
+  organisationId: string
+  role: Role
+}
+
+export const generateAccessToken = (payload: JwtPayload): string => {
+  return jwt.sign(payload, env.jwtAccessSecret!, {
+    expiresIn: env.jwtAccessExpiresIn as StringValue
+  })
+}
+
+export const generateRefreshToken = (payload: JwtPayload): string => {
+  return jwt.sign(payload, env.jwtRefreshSecret!, {
+    expiresIn: env.jwtRefreshExpiresIn as StringValue
+  })
+}
+
+export const verifyAccessToken = (token: string): JwtPayload => {
+  try {
+    const decoded = jwt.verify(token, env.jwtAccessSecret!) as JwtPayload
+    return decoded
+  } catch {
+    throw new ApiError(401, "Invalid or expired access token")
+  }
+}
+
+export const verifyRefreshToken = (token: string): JwtPayload => {
+  try {
+    const decoded = jwt.verify(token, env.jwtRefreshSecret!) as JwtPayload
+    return decoded
+  } catch {
+    throw new ApiError(401, "Invalid or expired refresh token")
+  }
+}
+
+
