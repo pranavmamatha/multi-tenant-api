678e8745302d6dec541d401b6e67a25df234ea03
pranavmamatha <pranavranjithkannan@gmail.com>
2026-02-22 13:56:59 +0530

    add port configuration to environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET!,
   jwtRefreshSecret: process.env.JWT_REFRESH_SECRET!,
   jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN!,
-  jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN!
+  jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN!,
+  port: Number(process.env.PORT!)
 }
